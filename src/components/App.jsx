4431fcbfcef25b5399064e06ebae5ba5277ebbbb
khrystyna-shekel <christina.shekel@gmail.com>
2023-12-17 23:28:01 +0200

    mark-up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+        </Route>
+        <Route path="*" element={<h1>Go to home page!</h1>} />
+      </Routes>
+    </>
   );
 };
+
+export default App;
---
+++ src/services/movies.api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://www.themoviedb.org/3';
+const KEY = '7da5fd28555694da9dd5eed6b6d34230';
+
+export const fetchMovies = async (endpoint, query = '') => {
+  const params = new URLSearchParams({
+    api_key: KEY,
+    query,
+  });
+  const { data } = await axios.get(`${endpoint}&${params}`);
+  return data;
+};
