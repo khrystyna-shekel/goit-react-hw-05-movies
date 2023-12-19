ee91d6097edea421bd2fe1a333743a87facde662
khrystyna-shekel <christina.shekel@gmail.com>
2023-12-19 02:57:19 +0200

    rev-started

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovies } from 'services/movies.api';
 
 const Reviews = () => {
-  return <div>reviews</div>;
+  const { movieId } = useParams();
+  const [review, setReview] = useState({});
+
+  useEffect(() => {
+    const getReview = async () => {
+      try {
+        const res = await fetchMovies(`/movie/${movieId}/reviews`);
+        console.log(res);
+        setReview(res);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getReview();
+  }, [movieId]);
+
+  return (
+    <>
+      <section></section>
+    </>
+  );
 };
 
 export default Reviews;
